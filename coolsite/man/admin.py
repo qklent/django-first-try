ff84d10cf450454a8a66da28cfee1ee990950deb
qklent <maksimsilenko8@gmail.com>
2022-08-07 23:31:37 +0400

    first commit

---
+++ coolsite/man/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import *
+
+class ManAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+
+admin.site.register(Man,ManAdmin)
+admin.site.register(Category,CategoryAdmin)
