5c2cba1888c91557053721c980f32dc9b41013cf
qklent <maksimsilenko8@gmail.com>
2022-08-09 17:19:41 +0400

    15th lesson

--- coolsite/man/views.py
+++ coolsite/man/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from .models import *
 from .forms import *
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
@@ -10,31 +12,66 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
         ]
 
 
-def index(request):
-    posts = Man.objects.all()
-    context = {
-        'posts': posts,
-        'menu': menu,
-        'title': 'Главная страница',
-        'cat_selected': 0,
-    }
-    return render(request, 'man/index.html', context=context)
+class ManHome(ListView):
+    model = Man
+    template_name = 'man/index.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, obblect_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Главная страница'
+        context['cat_selected'] = 0
+        return context
+
+    def get_queryset(self):
+        return Man.objects.filter(is_published=True)
+
+
+class ManCategory(ListView):
+    model = Man
+    template_name = 'man/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Man.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
+        context['menu'] = menu
+        context['cat_selected'] = context['posts'][0].cat_id
+        return context
+
+
+class ShowPost(DetailView):
+    model = Man
+    template_name = 'man/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = context['post']
+        context['menu'] = menu
+        return context
+
+
+class AddPage(CreateView):
+    form_class = AddPostForm
+    template_name = 'man/addpage.html'
+    succes_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Добавление статьи'
+        context['menu'] = menu
+        return context
 
 
 def about(request):
     return render(request, 'man/about.html', {'menu': menu, 'title': 'о сайте'})
-
-
-def addpage(request):
-    if request.method == "POST":
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = AddPostForm()
-
-    return render(request, 'man/addpage.html', {'form': form, "menu": menu, 'title': 'Добавление статьи'})
 
 
 def contact(request):
@@ -47,31 +84,3 @@ def login(request):
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
-
-
-def show_post(request, post_slug):
-    post = get_object_or_404(Man, slug=post_slug)
-    context = {
-        'post': post,
-        'menu': menu,
-        'title': post.title,
-        'cat_selected': post.cat_id,
-
-    }
-
-    return render(request, 'man/post.html', context=context)
-
-
-def show_category(request, cat_slug):
-    posts = Man.objects.filter(cat__slug=cat_slug)
-
-    if len(posts) == 0:
-        raise Http404()
-
-    context = {
-        'posts': posts,
-        'menu': menu,
-        'title': 'Отображение по рубрикам',
-        'cat_selected': cat_slug,
-    }
-    return render(request, 'man/index.html', context=context)
